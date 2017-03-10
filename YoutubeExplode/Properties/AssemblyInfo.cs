ac33db6d5ada4c93634bef6303e9d8a08e90f886
Alexey Golub <tyrrrrrr@gmail.com>
2017-03-10 14:32:44 +0200

    Update version

--- YoutubeExplode/Properties/AssemblyInfo.cs
+++ YoutubeExplode/Properties/AssemblyInfo.cs
@@ -24,5 +24,5 @@ using System.Reflection;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.2.*")]
-[assembly: AssemblyFileVersion("2.2")]
+[assembly: AssemblyVersion("2.3.*")]
+[assembly: AssemblyFileVersion("2.3")]
