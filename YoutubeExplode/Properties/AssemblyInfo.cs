67b2c8318b98f811b90051b723d76d08c3dc243b
Alexey Golub <Tyrrrz@users.noreply.github.com>
2017-03-13 16:23:21 +0200

    Update project metadata

--- YoutubeExplode/Properties/AssemblyInfo.cs
+++ YoutubeExplode/Properties/AssemblyInfo.cs
@@ -1,28 +1,14 @@
-﻿using System.Resources;
+using System.Resources;
 using System.Reflection;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [assembly: AssemblyTitle("YoutubeExplode")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Youtube parser")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Tyrrrz")]
 [assembly: AssemblyProduct("YoutubeExplode")]
-[assembly: AssemblyCopyright("Copyright © Alexey Golub 2016-2017")]
+[assembly: AssemblyCopyright("Copyright © Alexey 'Tyrrrz' Golub 2016-2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: NeutralResourcesLanguage("en")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("2.3.*")]
 [assembly: AssemblyFileVersion("2.3")]
