acb1b6f20d14011d26371742f8401858972bd1a0
Alexey Golub <tyrrrrrr@gmail.com>
2017-03-16 20:27:08 +0200

    Improve tests by adding more asserts
    Also added a failing test which indicates a current bug

---
+++ YoutubeExplodeTests/Extensions.cs
@@ -0,0 +1,24 @@
+﻿namespace YoutubeExplode.Tests
+{
+    public static class Extensions
+    {
+        public static int GetStaticHashCode(this string str)
+        {
+            unchecked
+            {
+                int hash1 = (5381 << 16) + 5381;
+                int hash2 = hash1;
+
+                for (int i = 0; i < str.Length; i += 2)
+                {
+                    hash1 = ((hash1 << 5) + hash1) ^ str[i];
+                    if (i == str.Length - 1)
+                        break;
+                    hash2 = ((hash2 << 5) + hash2) ^ str[i + 1];
+                }
+
+                return hash1 + hash2 * 1566083941;
+            }
+        }
+    }
+}
