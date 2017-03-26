9fb49da82588a6d6d298c7de52e9dbe110c6be6a
Alexey Golub <tyrrrrrr@gmail.com>
2017-03-26 15:02:16 +0300

    Update version

--- YoutubeExplode/Properties/AssemblyInfo.cs
+++ YoutubeExplode/Properties/AssemblyInfo.cs
@@ -10,5 +10,5 @@ using System.Reflection;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: NeutralResourcesLanguage("en")]
-[assembly: AssemblyVersion("2.4")]
-[assembly: AssemblyFileVersion("2.4")]
+[assembly: AssemblyVersion("2.5")]
+[assembly: AssemblyFileVersion("2.5")]
