c405bccd0f1508b1625713c078b4baec0769ffc0
Alexey Golub <Tyrrrz@users.noreply.github.com>
2017-10-23 22:49:40 +0300

    Remove mentions of Youtube Red

--- YoutubeExplode/Exceptions/VideoRequiresPurchaseException.cs
+++ YoutubeExplode/Exceptions/VideoRequiresPurchaseException.cs
@@ -20,7 +20,7 @@ namespace YoutubeExplode.Exceptions
 
         /// <summary />
         public VideoRequiresPurchaseException(string videoId, string previewVideoId)
-            : base("The video is a paid Youtube Red video and cannot be processed")
+            : base("The video requires purchase and cannot be processed")
         {
             VideoId = videoId.GuardNotNull(nameof(videoId));
             PreviewVideoId = previewVideoId.GuardNotNull(nameof(previewVideoId));
