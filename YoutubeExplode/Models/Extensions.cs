356bbd02458bf95990c86061972521320f61d4a4
Bruno Zell <bruno.zzell@gmail.com>
2017-12-02 21:27:30 +0100

    Add extension methods to get URLs for videos (#69)

--- YoutubeExplode/Models/Extensions.cs
+++ YoutubeExplode/Models/Extensions.cs
@@ -23,5 +23,59 @@ namespace YoutubeExplode.Models
             foreach (var streamInfo in video.VideoStreamInfos)
                 yield return streamInfo;
         }
+
+        /// <summary>
+        /// Generates the regular url of the YouTube watch page for this video
+        /// </summary>
+        public static string GetRegularUrl(this Video video)
+        {
+            video.GuardNotNull(nameof(video));
+            return $"https://www.youtube.com/watch?v={video.Id}";
+        }
+
+        /// <summary>
+        /// Generates the short url of the YouTube watch page for this video
+        /// </summary>
+        public static string GetShortUrl(this Video video)
+        {
+            video.GuardNotNull(nameof(video));
+            return $"https://youtu.be/{video.Id}";
+        }
+
+        /// <summary>
+        /// Generates the url of the embedded YouTube watch page for this video
+        /// </summary>
+        public static string GetEmbedUrl(this Video video)
+        {
+            video.GuardNotNull(nameof(video));
+            return $"https://www.youtube.com/embed/{video.Id}";
+        }
+
+        /// <summary>
+        /// Generates the regular url of the YouTube watch page for this video
+        /// </summary>
+        public static string GetRegularUrl(this PlaylistVideo playlistVideo)
+        {
+            playlistVideo.GuardNotNull(nameof(playlistVideo));
+            return $"https://www.youtube.com/watch?v={playlistVideo.Id}";
+        }
+
+        /// <summary>
+        /// Generates the short url of the YouTube watch page for this video
+        /// </summary>
+        public static string GetShortUrl(this PlaylistVideo playlistVideo)
+        {
+            playlistVideo.GuardNotNull(nameof(playlistVideo));
+            return $"https://youtu.be/{playlistVideo.Id}";
+        }
+
+        /// <summary>
+        /// Generates the url of the embedded YouTube watch page for this video
+        /// </summary>
+        public static string GetEmbedUrl(this PlaylistVideo playlistVideo)
+        {
+            playlistVideo.GuardNotNull(nameof(playlistVideo));
+            return $"https://www.youtube.com/embed/{playlistVideo.Id}";
+        }
     }
 }
