097f527d349f93934c4a12c70b7f5200042fcfa6
Oleksii Holub <Oleksii.Holub@3shape.com>
2018-07-24 16:23:19 +0300

    Add AsyncMultiStream to aggregate segmented streams into one stream
    By extension, GetMediaStreamAsync will now take care of rate-limited streams as well
    Closes #135

---
+++ YoutubeExplode/Internal/AsyncMultiStream.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace YoutubeExplode.Internal
+{
+    internal class AsyncMultiStream : Stream
+    {
+        private readonly Queue<Func<Task<Stream>>> _queue;
+        private Stream _currentStream;
+
+        public override bool CanRead => true;
+        public override bool CanSeek => false;
+        public override bool CanWrite => false;
+
+        public AsyncMultiStream(IEnumerable<Func<Task<Stream>>> streamTaskResolvers)
+        {
+            _queue = new Queue<Func<Task<Stream>>>(streamTaskResolvers);
+        }
+
+        public override async Task<int> ReadAsync(byte[] buffer, int offset, int count,
+            CancellationToken cancellationToken)
+        {
+            // If queue is empty - return
+            if (!_queue.Any())
+                return 0;
+
+            // If current stream is not set - resolve it
+            if (_currentStream == null)
+                _currentStream = await _queue.Dequeue().Invoke().ConfigureAwait(false);
+
+            // Read from stream
+            var bytesRead = await _currentStream.ReadAsync(buffer, offset, count, cancellationToken)
+                .ConfigureAwait(false);
+
+            // If nothing was read - move to next stream
+            if (bytesRead == 0)
+            {
+                // Dispose and nullify
+                _currentStream.Dispose();
+                _currentStream = null;
+
+                // Recursively read next stream
+                bytesRead += await ReadAsync(buffer, offset + bytesRead, count - bytesRead, cancellationToken)
+                    .ConfigureAwait(false);
+            }
+
+            return bytesRead;
+        }
+
+        public override int Read(byte[] buffer, int offset, int count) =>
+            ReadAsync(buffer, offset, count).GetAwaiter().GetResult();
+
+        protected override void Dispose(bool disposing)
+        {
+            _currentStream?.Dispose();
+
+            base.Dispose(disposing);
+        }
+
+        #region Not supported
+
+        public override long Length => throw new NotSupportedException();
+
+        public override long Position
+        {
+            get => throw new NotSupportedException();
+            set => throw new NotSupportedException();
+        }
+
+        public override void Flush() => throw new NotSupportedException();
+
+        public override long Seek(long offset, SeekOrigin origin) => throw new NotSupportedException();
+
+        public override void SetLength(long value) => throw new NotSupportedException();
+
+        public override void Write(byte[] buffer, int offset, int count) => throw new NotSupportedException();
+
+        #endregion
+    }
+}
--- YoutubeExplode/YoutubeClient.MediaStream.cs
+++ YoutubeExplode/YoutubeClient.MediaStream.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Text.RegularExpressions;
 using System.Threading;
@@ -15,10 +16,40 @@ namespace YoutubeExplode
         {
             info.GuardNotNull(nameof(info));
 
-            // Get stream
-            var stream = await _httpClient.GetStreamAsync(info.Url).ConfigureAwait(false);
+            // Determine if stream is rate-limited
+            var isRateLimited = !Regex.IsMatch(info.Url, @"ratebypass[=/]yes");
 
-            return new MediaStream(info, stream);
+            // If rate-limited - split into segments and wrap into one stream
+            if (isRateLimited)
+            {
+                // Determine segment count
+                const long segmentSize = 9_898_989; // this number was carefully devised through research
+                var segmentCount = (int) Math.Ceiling(1.0 * info.Size / segmentSize);
+
+                // Create resolvers for multi stream
+                var resolvers = new List<Func<Task<Stream>>>();
+                for (var i = 0; i < segmentCount; i++)
+                {
+                    // Determine segment range
+                    var from = i * segmentSize;
+                    var to = (i + 1) * segmentSize - 1;
+
+                    // Create resolver for this segment
+                    var resolver = new Func<Task<Stream>>(() => _httpClient.GetStreamAsync(info.Url, from, to));
+                    resolvers.Add(resolver);
+                }
+
+                // Create multi stream from segment resolvers
+                var stream = new AsyncMultiStream(resolvers);
+                return new MediaStream(info, stream);
+            }
+            // If not rate-limited - get the stream directly
+            else
+            {
+                // Get full stream
+                var stream = await _httpClient.GetStreamAsync(info.Url).ConfigureAwait(false);
+                return new MediaStream(info, stream);
+            }
         }
 
         /// <inheritdoc />
@@ -28,47 +59,8 @@ namespace YoutubeExplode
             info.GuardNotNull(nameof(info));
             output.GuardNotNull(nameof(output));
 
-            // Determine if stream is rate-limited
-            var isRateLimited = !Regex.IsMatch(info.Url, @"ratebypass[=/]yes");
-
-            // Download rate-limited streams in segments
-            if (isRateLimited)
-            {
-                // Determine segment count
-                const long segmentSize = 9_898_989; // this number was carefully devised through research
-                var segmentCount = (int) Math.Ceiling(1.0 * info.Size / segmentSize);
-
-                // Keep track of bytes copied for progress reporting
-                var totalBytesCopied = 0L;
-
-                for (var i = 0; i < segmentCount; i++)
-                {
-                    // Determine segment range
-                    var from = i * segmentSize;
-                    var to = (i + 1) * segmentSize - 1;
-
-                    // Download segment
-                    using (var input = await _httpClient.GetStreamAsync(info.Url, from, to).ConfigureAwait(false))
-                    {
-                        int bytesCopied;
-                        do
-                        {
-                            // Copy
-                            bytesCopied = await input.CopyChunkToAsync(output, cancellationToken).ConfigureAwait(false);
-
-                            // Report progress
-                            totalBytesCopied += bytesCopied;
-                            progress?.Report(1.0 * totalBytesCopied / info.Size);
-                        } while (bytesCopied > 0);
-                    }
-                }
-            }
-            // Download non-limited streams directly
-            else
-            {
-                using (var input = await GetMediaStreamAsync(info).ConfigureAwait(false))
-                    await input.CopyToAsync(output, progress, cancellationToken).ConfigureAwait(false);
-            }
+            using (var input = await GetMediaStreamAsync(info).ConfigureAwait(false))
+                await input.CopyToAsync(output, progress, cancellationToken).ConfigureAwait(false);
         }
 
 #if NETSTANDARD2_0 || NET45 || NETCOREAPP1_0
