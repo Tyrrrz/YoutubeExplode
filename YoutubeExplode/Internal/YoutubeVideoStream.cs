49c6fa8393ccf4c9ea3b7f8b49efa34202907e41
SlowLogicBoy <SlowLogicBoy@users.noreply.github.com>
2018-10-10 15:14:55 +0300

    Change media stream to seekable one

---
+++ YoutubeExplode/Internal/YoutubeVideoStream.cs
@@ -0,0 +1,66 @@
+using System;
+using System.IO;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace YoutubeExplode.Internal
+{
+    internal class YoutubeVideoStream : Stream
+    {
+        private readonly HttpClient _httpClient;
+        private const long MaxSegmentSize = 9_898_989;
+        private readonly string _url;
+        private readonly long _size;
+        public YoutubeVideoStream(HttpClient httpClient, string url, long size)
+        {
+            _url = url;
+            _httpClient = httpClient;
+            _size = size;
+        }
+
+        public override bool CanRead => true;
+
+        public override bool CanSeek => true;
+
+        public override bool CanWrite => false;
+
+        public override long Length => _size;
+
+        public override long Position { get; set; }
+
+        public override void Flush() => throw new System.NotSupportedException();
+        public override async Task<int> ReadAsync(byte[] buffer, int offset, int count, CancellationToken cancellationToken)
+        {
+            if (count > MaxSegmentSize - 1)
+                count = (int)MaxSegmentSize - 1;
+            var currentStream = await _httpClient.GetStreamAsync(_url, Position, Position + count).ConfigureAwait(false);
+            var bytesRead = await currentStream.ReadAsync(buffer, offset, count, cancellationToken).ConfigureAwait(false);
+            Position += bytesRead;
+            return bytesRead;
+        }
+        public override int Read(byte[] buffer, int offset, int count) =>
+            ReadAsync(buffer, offset, count).GetAwaiter().GetResult();
+
+        public override long Seek(long offset, SeekOrigin origin)
+        {
+            switch (origin)
+            {
+                case SeekOrigin.Begin:
+                    Position = Math.Max(0, offset);
+                    break;
+                case SeekOrigin.Current:
+                    Position = Math.Max(0, Math.Min(Length, Position + offset));
+                    break;
+                case SeekOrigin.End:
+                    Position = Math.Max(0, Math.Min(Length, Length - offset));
+                    break;
+            }
+            return Position;
+        }
+
+        public override void SetLength(long value) => throw new System.NotSupportedException();
+
+        public override void Write(byte[] buffer, int offset, int count) => throw new System.NotSupportedException();
+    }
+}
--- YoutubeExplode/YoutubeClient.MediaStream.cs
+++ YoutubeExplode/YoutubeClient.MediaStream.cs
@@ -12,49 +12,10 @@ namespace YoutubeExplode
     public partial class YoutubeClient
     {
         /// <inheritdoc />
-        public async Task<MediaStream> GetMediaStreamAsync(MediaStreamInfo info)
+        public Task<MediaStream> GetMediaStreamAsync(MediaStreamInfo info)
         {
             info.GuardNotNull(nameof(info));
-
-            // Maximum segment stream size
-            const long segmentSize = 9_898_989; // this number was carefully devised through research
-
-            // Determine if stream is rate-limited
-            var isRateLimited = !Regex.IsMatch(info.Url, @"ratebypass[=/]yes");
-
-            // Determine if the stream is longer than one segment
-            var isMultiSegment = info.Size > segmentSize;
-
-            // If rate-limited and long enough - split into segments and wrap into one stream
-            if (isRateLimited && isMultiSegment)
-            {
-                // Determine segment count
-                var segmentCount = (int) Math.Ceiling(1.0 * info.Size / segmentSize);
-
-                // Create resolvers for multi stream
-                var resolvers = new List<Func<Task<Stream>>>();
-                for (var i = 0; i < segmentCount; i++)
-                {
-                    // Determine segment range
-                    var from = i * segmentSize;
-                    var to = (i + 1) * segmentSize - 1;
-
-                    // Create resolver for this segment
-                    var resolver = new Func<Task<Stream>>(() => _httpClient.GetStreamAsync(info.Url, from, to));
-                    resolvers.Add(resolver);
-                }
-
-                // Create multi stream from segment resolvers
-                var stream = new AsyncMultiStream(resolvers);
-                return new MediaStream(info, stream);
-            }
-            // If not rate-limited or not long enough - get the stream in one segment
-            else
-            {
-                // Get the whole stream as one segment
-                var stream = await _httpClient.GetStreamAsync(info.Url, 0, info.Size).ConfigureAwait(false);
-                return new MediaStream(info, stream);
-            }
+            return Task.FromResult(new MediaStream(info, new YoutubeVideoStream(_httpClient, info.Url, info.Size)));
         }
 
         /// <inheritdoc />
