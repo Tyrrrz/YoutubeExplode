bf547f77da3c211cfd63a80fb75ab6dc766cdaa4
Alexey Golub <Tyrrrrrr@gmail.com>
2019-03-03 22:18:03 +0200

    Make console demo even more simple

--- DemoConsole/Program.cs
+++ DemoConsole/Program.cs
@@ -1,7 +1,6 @@
 ﻿using System;
-using System.IO;
 using System.Threading.Tasks;
-using Tyrrrz.Extensions;
+using DemoConsole.Internal;
 using YoutubeExplode;
 using YoutubeExplode.Models.MediaStreams;
 
@@ -20,24 +19,6 @@ namespace DemoConsole
                 : input;
         }
 
-        /// <summary>
-        /// Turns file size in bytes into human-readable string.
-        /// </summary>
-        private static string NormalizeFileSize(long fileSize)
-        {
-            string[] units = { "B", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB" };
-            double size = fileSize;
-            var unit = 0;
-
-            while (size >= 1024)
-            {
-                size /= 1024;
-                ++unit;
-            }
-
-            return $"{size:0.#} {units[unit]}";
-        }
-
         private static async Task MainAsync()
         {
             // Client
@@ -47,46 +28,21 @@ namespace DemoConsole
             Console.Write("Enter YouTube video ID or URL: ");
             var videoId = Console.ReadLine();
             videoId = NormalizeVideoId(videoId);
-            Console.WriteLine();
-
-            // Get the video info
-            Console.Write("Obtaining general video info... ");
-            var video = await client.GetVideoAsync(videoId);
-            Console.WriteLine('✓');
-            Console.WriteLine($"> {video.Title} by {video.Author}");
-            Console.WriteLine();
 
             // Get media stream info set
-            Console.Write("Obtaining media stream info set... ");
             var streamInfoSet = await client.GetVideoMediaStreamInfosAsync(videoId);
-            Console.WriteLine('✓');
-            Console.WriteLine("> " +
-                              $"{streamInfoSet.Muxed.Count} muxed streams, " +
-                              $"{streamInfoSet.Video.Count} video-only streams, " +
-                              $"{streamInfoSet.Audio.Count} audio-only streams");
-            Console.WriteLine();
 
-            // Get the best muxed stream
+            // Choose the best muxed stream
             var streamInfo = streamInfoSet.Muxed.WithHighestVideoQuality();
-            Console.WriteLine("Selected muxed stream with highest video quality:");
-            Console.WriteLine("> " +
-                              $"{streamInfo.VideoQualityLabel} video quality | " +
-                              $"{streamInfo.Container} format | " +
-                              $"{NormalizeFileSize(streamInfo.Size)}");
-            Console.WriteLine();
 
             // Compose file name, based on metadata
             var fileExtension = streamInfo.Container.GetFileExtension();
-            var fileName = $"{video.Title}.{fileExtension}";
-
-            // Replace illegal characters in file name
-            fileName = fileName.Replace(Path.GetInvalidFileNameChars(), '_');
+            var fileName = $"{videoId}.{fileExtension}";
 
             // Download video
-            Console.Write("Downloading... ");
-            using (var progress = new ProgressBar())
+            Console.Write($"Downloading stream: {streamInfo.VideoQualityLabel} / {fileExtension}... ");
+            using (var progress = new InlineProgress())
                 await client.DownloadMediaStreamAsync(streamInfo, fileName, progress);
-            Console.WriteLine();
 
             Console.WriteLine($"Video saved to '{fileName}'");
             Console.ReadKey();
@@ -94,7 +50,7 @@ namespace DemoConsole
 
         public static void Main(string[] args)
         {
-            // This demo prompts for video ID, gets video info and downloads one media stream
+            // This demo prompts for video ID and downloads one media stream
             // It's intended to be very simple and straight to the point
             // For a more complicated example - check out the WPF demo
 
