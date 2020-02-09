4ec3e0f843497479d5e4aff3b64fe430c5416f7c
polynoman <polynoman@live.de>
2020-02-09 22:36:18 +0100

    Set playlist index to 0 for all cases.

--- YoutubeExplode/YoutubeClient.Playlist.cs
+++ YoutubeExplode/YoutubeClient.Playlist.cs
@@ -28,7 +28,7 @@ namespace YoutubeExplode
             // Get all videos across pages
             JToken playlistJson;
             var page = 1;
-            var index = playlistId.StartsWith("PL", StringComparison.OrdinalIgnoreCase) ? 101 : 0;
+            var index = 0;
             var videoIds = new HashSet<string>();
             var videos = new List<Video>();
             do
