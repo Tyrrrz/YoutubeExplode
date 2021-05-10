970041608ac5e26fb8174e2fd0a09bee082f4cfd
Tyrrrz <tyrrrrrr@gmail.com>
2021-05-10 17:43:22 +0300

    Ignore rawcc dash streams
    Fixes #546

--- YoutubeExplode/Bridge/Extractors/DashManifestExtractor.cs
+++ YoutubeExplode/Bridge/Extractors/DashManifestExtractor.cs
@@ -15,12 +15,21 @@ namespace YoutubeExplode.Bridge.Extractors
         public IReadOnlyList<IStreamInfoExtractor> GetStreams() => _memo.Wrap(() =>
             _content
                 .Descendants("Representation")
+                // Skip non-media representations (like "rawcc")
+                // https://github.com/Tyrrrz/YoutubeExplode/issues/546
+                .Where(x => x
+                    .Attribute("id")?
+                    .Value
+                    .All(char.IsDigit) == true)
+                // Skip segmented streams
+                // https://github.com/Tyrrrz/YoutubeExplode/issues/159
                 .Where(x => x
                     .Descendants("Initialization")
                     .FirstOrDefault()?
                     .Attribute("sourceURL")?
                     .Value
                     .Contains("sq/") != true)
+                // Skip streams without codecs
                 .Where(x => !string.IsNullOrWhiteSpace(x.Attribute("codecs")?.Value))
                 .Select(x => new DashStreamInfoExtractor(x))
                 .ToArray()
