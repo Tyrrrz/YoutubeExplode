601c30df1e39d488f2e24e09b9fa6fd5a9f01dd0
Tyrrrz <1935960+Tyrrrz@users.noreply.github.com>
2021-12-09 02:31:36 +0200

    Refactor `Json.Extract(...)`

--- YoutubeExplode/Utils/Json.cs
+++ YoutubeExplode/Utils/Json.cs
@@ -9,36 +9,27 @@ internal static class Json
     public static string Extract(string source)
     {
         var buffer = new StringBuilder();
-        var depth = 0;
 
-        var insideString = false;
+        var depth = 0;
+        var isInsideString = false;
 
         // We trust that the source contains valid json, we just need to extract it.
         // To do it, we will be matching curly braces until we even out.
         for (var i = 0; i < source.Length; i++)
         {
             var ch = source[i];
-            var chNxt = i < source.Length - 1 ? source[i + 1] : default;
+            var chPrv = i > 0 ? source[i - 1] : default;
 
             buffer.Append(ch);
 
-            if (ch == '\\' && insideString && chNxt != default)
-            {
-                // we've come across an escaped character.
-                // consume it and the next character.
-                buffer.Append(chNxt);
-                i++; // skip the next character on the loop.
-                continue;
-            }
-
-            // an unescaped quote, toggle our inside string status
-            if (ch == '"')
-                insideString = !insideString;
-
-            // Match braces that are not inside strings
-            if (ch == '{' && !insideString)
+            // Detect if inside string
+            if (ch == '"' && chPrv != '\\')
+                isInsideString = !isInsideString;
+            // Opening brace
+            else if (ch == '{' && !isInsideString)
                 depth++;
-            else if (ch == '}' && !insideString)
+            // Closing brace
+            else if (ch == '}' && !isInsideString)
                 depth--;
 
             // Break when evened out
