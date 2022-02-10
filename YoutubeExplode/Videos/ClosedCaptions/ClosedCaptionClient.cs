9444a2141136a56fb25de0f1fa9bedbdde62e9bf
AlmightyLks <AlmightyLks@protonmail.com>
2022-02-10 19:05:34 +0100

    Caption-syntax errors for duration will be disregarded rather than throwing an exception, added test case for such

--- YoutubeExplode/Videos/ClosedCaptions/ClosedCaptionClient.cs
+++ YoutubeExplode/Videos/ClosedCaptions/ClosedCaptionClient.cs
@@ -78,13 +78,13 @@ public class ClosedCaptionClient
                 if (string.IsNullOrWhiteSpace(text))
                     return null;
 
+                if (c.TryGetDuration() is not { } duration) // elegant null check and gets rid of nullable quick
+                    return null;
+
                 var offset =
                     c.TryGetOffset() ??
                     throw new YoutubeExplodeException("Could not extract caption offset.");
 
-                var duration =
-                    c.TryGetDuration() ??
-                    throw new YoutubeExplodeException("Could not extract caption duration.");
 
                 var parts = c
                     .GetParts()
