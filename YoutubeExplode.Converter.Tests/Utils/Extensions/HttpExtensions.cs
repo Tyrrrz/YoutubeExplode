482d5a4d442f701cb3224212088a739c748c5e59
Tyrrrz <1935960+Tyrrrz@users.noreply.github.com>
2023-06-18 20:51:01 +0300

    Verify hash when downloading FFmpeg in tests

---
+++ YoutubeExplode.Converter.Tests/Utils/Extensions/HttpExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System.IO;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace YoutubeExplode.Converter.Tests.Utils.Extensions;
+
+internal static class HttpExtensions
+{
+    public static async Task DownloadAsync(this HttpClient httpClient, string url, string filePath)
+    {
+        using var response = await httpClient.GetAsync(url, HttpCompletionOption.ResponseHeadersRead);
+        response.EnsureSuccessStatusCode();
+
+        await using var source = await response.Content.ReadAsStreamAsync();
+        await using var destination = File.Create(filePath);
+
+        await source.CopyToAsync(destination);
+    }
+}
--- YoutubeExplode.Converter.Tests/Utils/FFmpeg.cs
+++ YoutubeExplode.Converter.Tests/Utils/FFmpeg.cs
@@ -1,17 +1,22 @@
 ﻿using System;
 using System.IO;
 using System.IO.Compression;
+using System.Linq;
 using System.Net.Http;
 using System.Reflection;
 using System.Runtime.InteropServices;
+using System.Security.Cryptography;
 using System.Threading;
 using System.Threading.Tasks;
+using YoutubeExplode.Converter.Tests.Utils.Extensions;
 
 namespace YoutubeExplode.Converter.Tests.Utils;
 
 public static class FFmpeg
 {
     private static readonly SemaphoreSlim Lock = new(1, 1);
+
+    public static Version Version { get; } = new(4, 4, 1);
 
     private static string FileName { get; } =
         RuntimeInformation.IsOSPlatform(OSPlatform.Windows)
@@ -45,7 +50,7 @@ public static class FFmpeg
                 return "64";
 
             if (RuntimeInformation.ProcessArchitecture == Architecture.X86)
-                return "86";
+                return "32";
 
             if (RuntimeInformation.ProcessArchitecture == Architecture.Arm64)
                 return "arm-64";
@@ -56,28 +61,79 @@ public static class FFmpeg
             throw new NotSupportedException("Unsupported architecture.");
         }
 
-        const string version = "4.4.1";
         var plat = GetPlatformMoniker();
         var arch = GetArchitectureMoniker();
 
-        return $"https://github.com/vot/ffbinaries-prebuilt/releases/download/v{version}/ffmpeg-{version}-{plat}-{arch}.zip";
+        return $"https://github.com/vot/ffbinaries-prebuilt/releases/download/v{Version}/ffmpeg-{Version}-{plat}-{arch}.zip";
+    }
+
+    private static byte[] GetDownloadHash()
+    {
+        static string GetHashString()
+        {
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                // Only x64 build is available
+                return "d1124593b7453fc54dd90ca3819dc82c22ffa957937f33dd650082f1a495b10e";
+            }
+
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+            {
+                if (RuntimeInformation.ProcessArchitecture == Architecture.X64)
+                    return "4348301b0d5e18174925e2022da1823aebbdb07282bbe9adb64b2485e1ef2df7";
+
+                if (RuntimeInformation.ProcessArchitecture == Architecture.X86)
+                    return "a292731806fe3733b9e2281edba881d1035e4018599577174a54e275c0afc931";
+
+                if (RuntimeInformation.ProcessArchitecture == Architecture.Arm64)
+                    return "7d57e730cc34208743cc1a97134541656ecd2c3adcdfad450dedb61d465857da";
+            }
+
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+            {
+                // Only x64 build is available
+                return "e08c670fcbdc2e627aa4c0d0c5ee1ef20e82378af2f14e4e7ae421a148bd49af";
+            }
+
+            throw new NotSupportedException("Unsupported architecture.");
+        }
+
+        var hashString = GetHashString();
+
+        return Enumerable
+            .Range(0, hashString.Length)
+            .Where(x => x % 2 == 0)
+            .Select(x => Convert.ToByte(hashString.Substring(x, 2), 16))
+            .ToArray();
     }
 
     private static async ValueTask DownloadAsync()
     {
+        using var archiveFile = TempFile.Create();
         using var httpClient = new HttpClient();
 
-        // Extract the FFmpeg binary from the downloaded archive
-        await using var zipStream = await httpClient.GetStreamAsync(GetDownloadUrl());
-        using var zip = new ZipArchive(zipStream, ZipArchiveMode.Read);
+        // Download the archive
+        await httpClient.DownloadAsync(GetDownloadUrl(), archiveFile.Path);
 
-        var entry = zip.GetEntry(FileName);
-        if (entry is null)
-            throw new FileNotFoundException("Downloaded archive doesn't contain FFmpeg.");
+        // Verify the hash
+        await using (var archiveStream = File.OpenRead(archiveFile.Path))
+        {
+            var expectedHash = GetDownloadHash();
+            var actualHash = await SHA256.HashDataAsync(archiveStream);
 
-        await using var entryStream = entry.Open();
-        await using var fileStream = File.Create(FilePath);
-        await entryStream.CopyToAsync(fileStream);
+            if (!actualHash.SequenceEqual(expectedHash))
+                throw new Exception("Downloaded archive has invalid hash.");
+        }
+
+        // Extract the executable
+        using (var zip = ZipFile.OpenRead(archiveFile.Path))
+        {
+            var entry =
+                zip.GetEntry(FileName) ??
+                throw new FileNotFoundException("Downloaded archive doesn't contain the FFmpeg executable.");
+
+            entry.ExtractToFile(FilePath, true);
+        }
 
         // Add the execute permission on Unix
         if (!RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
