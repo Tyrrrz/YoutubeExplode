461c672c3fc49e8fe98d0e51ff0d84a886ceb69b
Tyrrrz <1935960+Tyrrrz@users.noreply.github.com>
2023-07-30 14:55:33 +0300

    Make `ToHex()` more versatile

--- YoutubeExplode/Utils/Extensions/BinaryExtensions.cs
+++ YoutubeExplode/Utils/Extensions/BinaryExtensions.cs
@@ -4,12 +4,16 @@ namespace YoutubeExplode.Utils.Extensions;
 
 internal static class BinaryExtensions
 {
-    public static string ToHex(this byte[] data)
+    public static string ToHex(this byte[] data, bool isUpperCase = true)
     {
         var buffer = new StringBuilder(2 * data.Length);
 
         foreach (var b in data)
-            buffer.Append(b.ToString("x2"));
+        {
+            buffer.Append(
+                b.ToString(isUpperCase ? "X2" : "x2")
+            );
+        }
 
         return buffer.ToString();
     }
