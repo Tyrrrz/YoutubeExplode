f4ff634f9dfebd5446f5a27a7f318b0adbf7bdff
Tyrrrz <1935960+Tyrrrz@users.noreply.github.com>
2023-08-27 03:52:28 +0300

    Don't dispose original request content when cloning requests

--- YoutubeExplode/Utils/Extensions/HttpExtensions.cs
+++ YoutubeExplode/Utils/Extensions/HttpExtensions.cs
@@ -1,4 +1,6 @@
-﻿using System.Net.Http;
+﻿using System.IO;
+using System.Net;
+using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -6,12 +8,34 @@ namespace YoutubeExplode.Utils.Extensions;
 
 internal static class HttpExtensions
 {
+    private class NonDisposableHttpContent : HttpContent
+    {
+        private readonly HttpContent _content;
+
+        public NonDisposableHttpContent(HttpContent content) => _content = content;
+
+        protected override async Task SerializeToStreamAsync(
+            Stream stream,
+            TransportContext? context
+        ) => await _content.CopyToAsync(stream);
+
+        protected override bool TryComputeLength(out long length)
+        {
+            length = default;
+            return false;
+        }
+    }
+
     public static HttpRequestMessage Clone(this HttpRequestMessage request)
     {
-        var clonedRequest = new HttpRequestMessage(request.Method, request.RequestUri);
-
-        clonedRequest.Content = request.Content;
-        clonedRequest.Version = request.Version;
+        var clonedRequest = new HttpRequestMessage(request.Method, request.RequestUri)
+        {
+            Version = request.Version,
+            // Don't dispose the original request's content
+            Content = request.Content is not null
+                ? new NonDisposableHttpContent(request.Content)
+                : null
+        };
 
         foreach (var (key, value) in request.Headers)
             clonedRequest.Headers.TryAddWithoutValidation(key, value);
