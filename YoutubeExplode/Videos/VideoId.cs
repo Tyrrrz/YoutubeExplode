81e37216fd5864290fa357b8940f807ec9b35c0d
Oleksii Holub <1935960+Tyrrrz@users.noreply.github.com>
2023-09-22 14:08:22 +0300

    Update YoutubeExplode/Videos/VideoId.cs

--- YoutubeExplode/Videos/VideoId.cs
+++ YoutubeExplode/Videos/VideoId.cs
@@ -73,14 +73,14 @@ public partial struct VideoId
         if (!string.IsNullOrWhiteSpace(shortsMatch) && IsValid(shortsMatch))
             return shortsMatch;
 
-        // Live Share URL
+        // Live URL
         // https://www.youtube.com/live/jfKfPfyJRdk
-        var liveShareMatch = Regex
+        var liveMatch = Regex
             .Match(videoIdOrUrl, @"youtube\..+?/live/(.*?)(?:\?|&|/|$)")
             .Groups[1].Value.Pipe(WebUtility.UrlDecode);
 
-        if (!string.IsNullOrWhiteSpace(liveShareMatch) && IsValid(liveShareMatch))
-            return liveShareMatch;
+        if (!string.IsNullOrWhiteSpace(liveMatch) && IsValid(liveMatch))
+            return liveMatch;
 
         // Invalid input
         return null;
