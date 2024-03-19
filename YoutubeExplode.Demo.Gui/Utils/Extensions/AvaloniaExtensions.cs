69b6dbef4e232a63656075d635dd0e901f01cea3
Tyrrrz <1935960+Tyrrrz@users.noreply.github.com>
2024-03-20 00:23:06 +0200

    Fix visual root retrieval in demo

--- YoutubeExplode.Demo.Gui/Utils/Extensions/AvaloniaExtensions.cs
+++ YoutubeExplode.Demo.Gui/Utils/Extensions/AvaloniaExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using Avalonia.Controls;
 using Avalonia.Controls.ApplicationLifetimes;
+using Avalonia.VisualTree;
 
 namespace YoutubeExplode.Demo.Gui.Utils.Extensions;
 
@@ -7,5 +8,5 @@ internal static class AvaloniaExtensions
 {
     public static TopLevel? TryGetTopLevel(this IApplicationLifetime lifetime) =>
         (lifetime as IClassicDesktopStyleApplicationLifetime)?.MainWindow
-        ?? (lifetime as ISingleViewApplicationLifetime)?.MainView as TopLevel;
+        ?? (lifetime as ISingleViewApplicationLifetime)?.MainView?.GetVisualRoot() as TopLevel;
 }
