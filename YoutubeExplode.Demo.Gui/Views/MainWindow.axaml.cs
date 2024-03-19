d3aacd708316911ac80cd1bd8a7fa90bf1c20b69
Tyrrrz <1935960+Tyrrrz@users.noreply.github.com>
2024-03-19 23:29:31 +0200

    Use expression-bodied members

--- YoutubeExplode.Demo.Gui/Views/MainWindow.axaml.cs
+++ YoutubeExplode.Demo.Gui/Views/MainWindow.axaml.cs
@@ -5,13 +5,7 @@ namespace YoutubeExplode.Demo.Gui.Views;
 
 public partial class MainWindow : Window
 {
-    public MainWindow()
-    {
-        InitializeComponent();
-    }
+    public MainWindow() => InitializeComponent();
 
-    private void Root_OnLoaded(object? sender, RoutedEventArgs args)
-    {
-        QueryTextBox.Focus();
-    }
+    private void Root_OnLoaded(object? sender, RoutedEventArgs args) => QueryTextBox.Focus();
 }
