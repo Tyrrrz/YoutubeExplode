c6c10f272f95f62ab144000879de85a18c2a6992
Tyrrrz <1935960+Tyrrrz@users.noreply.github.com>
2024-12-02 16:31:16 +0200

    asd

--- YoutubeExplode/Videos/Streams/IAudioStreamInfo.cs
+++ YoutubeExplode/Videos/Streams/IAudioStreamInfo.cs
@@ -16,7 +16,7 @@ public interface IAudioStreamInfo : IStreamInfo
     /// Audio language.
     /// </summary>
     /// <remarks>
-    /// May be null if the audio stream does not have language information.
+    /// May be null if the audio stream does not contain language information.
     /// </remarks>
     Language? AudioLanguage { get; }
 
@@ -24,7 +24,7 @@ public interface IAudioStreamInfo : IStreamInfo
     /// Whether the audio stream's language corresponds to the default language of the video.
     /// </summary>
     /// <remarks>
-    /// May be null if the audio stream does not have language information.
+    /// May be null if the audio stream does not contain language information.
     /// </remarks>
     bool? IsAudioLanguageDefault { get; }
 }
