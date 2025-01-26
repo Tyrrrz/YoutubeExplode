5df2370111433ca85950154e674f2f9344327bb8
Tyrrrz <1935960+Tyrrrz@users.noreply.github.com>
2025-01-26 04:02:39 +0200

    More cleanup

--- YoutubeExplode/Bridge/VisitorData.cs
+++ YoutubeExplode/Bridge/VisitorData.cs
@@ -2,31 +2,26 @@
 using System.Linq;
 using System.Text;
 using YoutubeExplode.Utils;
+using YoutubeExplode.Utils.Extensions;
 
 namespace YoutubeExplode.Bridge;
 
 internal static class VisitorData
 {
     private static readonly Random Random = new();
-
-    private static string GenerateRandomString(int length)
-    {
-        const string alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_";
-
-        var buffer = new StringBuilder();
-        for (var i = 0; i < length; i++)
-            buffer.Append(alphabet.ElementAt(Random.Next(alphabet.Length)));
-
-        return buffer.ToString();
-    }
+    private static readonly char[] SessionIdAllowedChars =
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_".ToCharArray();
 
     public static string Generate() =>
         new ProtoBuilder()
-            .AddString(1, GenerateRandomString(11))
+            // Session ID
+            .AddString(1, Random.NextString(SessionIdAllowedChars, 11))
+            // Timestamp
             .AddNumber(
                 5,
                 DateTimeOffset.UtcNow.ToUnixTimeMilliseconds() / 1000 - Random.Next(600000)
             )
+            // Country info
             .AddBytes(
                 6,
                 new ProtoBuilder()
---
+++ YoutubeExplode/Utils/Extensions/RandomExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace YoutubeExplode.Utils.Extensions;
+
+internal static class RandomExtensions
+{
+    public static string NextString(this Random random, char[] allowedChars, int length)
+    {
+        var buffer = new char[length];
+        for (var i = 0; i < length; i++)
+            buffer[i] = allowedChars[random.Next(allowedChars.Length)];
+
+        return new string(buffer);
+    }
+}
