eec387d8cabd7776f349f01083305f66f8a99d09
Oleksii Holub <1935960+Tyrrrz@users.noreply.github.com>
2025-11-11 15:06:39 +0200

    Apply suggestion from @Tyrrrz

--- YoutubeExplode.Converter/Converter.cs
+++ YoutubeExplode.Converter/Converter.cs
@@ -99,7 +99,7 @@ internal partial class Converter(VideoClient videoClient, FFmpeg ffmpeg, Convers
             {
                 if (streamInput.Info is IAudioStreamInfo audioStreamInfo)
                 {
-                    // Have ffmpeg determine best quality possible
+                    // Have FFmpeg determine best quality possible
                     arguments.Add($"-q:a:{lastAudioStreamIndex++}").Add("0");
                 }
             }
